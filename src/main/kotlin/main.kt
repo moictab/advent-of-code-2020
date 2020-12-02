d09fcb06edf614e3c06ae93b31e7bdf03bf05cfc
Moisés Cano Torralba <moictab@gmail.com>
2020-12-02 11:59:52 +0100

    Add day 1 report-repair solution, second part

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,36 @@
 import java.io.File
 
 fun main() {
-    println("Day 1: " + ReportRepair().solve())
+    println("Day 1, first part: " + ReportRepair().solveFirstPart())
+    println("Day 2, second part: " + ReportRepair().solveSecondPart())
 }
 
 class ReportRepair {
     private val input: File = File(javaClass.getResource("input.txt").file)
     private val numbers = arrayListOf<Int>()
 
-    fun solve(): Int {
+    fun solveFirstPart(): Int {
         this.input.forEachLine { this.numbers.add(it.toInt()) }
         this.numbers.forEachIndexed { index, number ->
             run {
                 for (i in index until this.numbers.size) {
                     if (number + numbers[i] == 2020) {
                         return number * numbers[i]
+                    }
+                }
+            }
+        }
+
+        throw IllegalArgumentException("Invalid input")
+    }
+
+    fun solveSecondPart(): Int {
+        this.input.forEachLine { this.numbers.add(it.toInt()) }
+        for (i in 0 until numbers.size) {
+            for (j in i until numbers.size) {
+                for (k in j until numbers.size) {
+                    if (numbers[i] + numbers[j] + numbers[k] == 2020) {
+                        return numbers[i] * numbers[j] * numbers[k]
                     }
                 }
             }
